defb031aefca7e4b94075dc7483644214d1c52da
Chris Bannister <c.bannister@gmail.com>
2010-12-06 17:01:34 +0000

    Connect to astra!

--- leechz.cpp
+++ leechz.cpp
@@ -1,9 +1,11 @@
+#include <string>
+
 #include "usenet.h"
 
 int main(int argc, char **argv)
 {
-    //Usenet *conn = new Usenet("eu.news.astraweb.com", 119, 0);
-    Usenet *conn = new Usenet("localhost", 5000);
+    Usenet *conn = new Usenet("eu.news.astraweb.com", 119);
+    //Usenet *conn = new Usenet("localhost", 5000);
 
     delete conn;
 
