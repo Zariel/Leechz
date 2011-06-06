5d94835602c8bef74ead96e4f4922b7a969d28c0
Chris Bannister <c.bannister@gmail.com>
2011-06-06 21:08:25 +0100

    Wait for connection pool to init before doing things to it

--- leechz.cpp
+++ leechz.cpp
@@ -8,6 +8,10 @@ int main(int argc, char **argv)
     Connections *pool = new Connections("localhost", 5000, 0, 2);
     //Connections *pool = new Connections("eu.news.astraweb.com", 119);
     //Usenet *conn = new Usenet("localhost", 5000);
+    /* Let it init */
+    pool->init_lock->acquire();
+    pool->init_lock->release();
+
     pool->kill_lock->release();
 
     pool->join();
